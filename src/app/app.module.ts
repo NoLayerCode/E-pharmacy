21fdedcf3db7c6c3efe2ced4f0663f2f29928885
NoLayerCode <vyelhekar@gmail.com>
2019-12-01 02:30:56 +0530

    updated the components and their services tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,8 @@ import { AdminComponent } from './admin/admin.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
 import { ProductsComponent } from './products/products.component';
-import { ProductServicesComponent } from './products/product-services/product-services.component';
-import { CartServicesComponent } from './products/product-services/cart-services/cart-services.component';
-import { CartComponent } from './products/cart/cart.component';
-import { UsersComponent } from './users/users.component';
-import { ServicesComponent } from './users/services/services.component';
-import { UserServicesComponent } from './users/services/user-services/user-services.component';
-import { SignInComponent } from './users/sign-in/sign-in.component';
-import { SignUpComponent } from './users/sign-up/sign-up.component';
-
+import { SigninComponent } from './users/signin/signin.component';
+import { SignupComponent } from './users/signup/signup.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,20 +16,19 @@ import { SignUpComponent } from './users/sign-up/sign-up.component';
     AdminProductsComponent,
     AdminUsersComponent,
     ProductsComponent,
-    ProductServicesComponent,
-    CartServicesComponent,
     CartComponent,
-    UsersComponent,
-    ServicesComponent,
-    UserServicesComponent,
-    SignInComponent,
-    SignUpComponent
+    SigninComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // const routes: Routes = [
+  //   {path: '', component: UsersComponent }
+  // ]
+
 })
 export class AppModule { }
--- src/app/users/signin/signin.component.spec.ts
+++ src/app/users/signin/signin.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ServicesComponent } from './services.component';
+import { SigninComponent } from './signin.component';
 
-describe('ServicesComponent', () => {
-  let component: ServicesComponent;
-  let fixture: ComponentFixture<ServicesComponent>;
+describe('SigninComponent', () => {
+  let component: SigninComponent;
+  let fixture: ComponentFixture<SigninComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServicesComponent ]
+      declarations: [ SigninComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ServicesComponent);
+    fixture = TestBed.createComponent(SigninComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
